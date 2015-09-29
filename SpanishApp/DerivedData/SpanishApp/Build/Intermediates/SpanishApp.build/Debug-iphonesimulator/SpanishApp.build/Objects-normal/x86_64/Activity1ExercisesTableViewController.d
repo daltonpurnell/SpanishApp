c22ffe0753fc178e072851679f601b2cce6f2448
daltonpurnell <daltonpurnell@live.com>
2015-09-29 10:01:54 -0600

    sets up first 2 views

---
+++ SpanishApp/DerivedData/SpanishApp/Build/Intermediates/SpanishApp.build/Debug-iphonesimulator/SpanishApp.build/Objects-normal/x86_64/Activity1ExercisesTableViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/djp1989/Documents/Development/PersonalProjects/SpanishApp/SpanishApp/SpanishApp/Activity1ExercisesTableViewController.m \
+  /Users/djp1989/Documents/Development/PersonalProjects/SpanishApp/SpanishApp/SpanishApp/Activity1ExercisesTableViewController.h
---
+++ SpanishApp/SpanishApp/Activity1ExercisesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  Activity1ExercisesTableViewController.h
+//  SpanishApp
+//
+//  Created by Dalton on 9/29/15.
+//  Copyright © 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Activity1ExercisesTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
